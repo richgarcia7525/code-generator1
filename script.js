fb466698e96329573d529458457e8188dd40a7e2
BITCHARD <richgarcia7525@gmail.com>
2021-11-21 08:22:21 -0600

    code-generator1
    a program that helps you to generate a new password containing upper, lower, symbols and numbers of your choice.

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ generateBtn.addEventListener("click", writePassword);
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  //setting generate password in HTML
+  passwordText.value = password;
+}
+function generatePassword() {
+  //asking the user for the number of charcters
+  const length = prompt("How many characters in password");
+  if (!length || isNaN(length) || length < 8 || length > 128) {
+    //if user does not meet conditions return null
+    alert("password length must be a numeric value between 8 and 128");
+    return "";
+  }
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  //setting generate password in HTML
   passwordText.value = password;
 }
 
